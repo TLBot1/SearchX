97d0f30b06051c0233f50fb1bf35a7bb24975bc9
TLBot1 <96611565+TLBot1@users.noreply.github.com>
2022-04-17 11:52:23 +0530

    Hubdrive fix

--- bot/helper/ext_utils/parser.py
+++ bot/helper/ext_utils/parser.py
@@ -141,44 +141,25 @@ def sharer(url: str, forced_login=False) -> str:
     else:
         raise DDLException("Invalid link!")
 
-def parse_info(res):
-    info_parsed = {}
-    title = re.findall('>(.*?)<\/h4>', res.text)[0]
-    info_chunks = re.findall('>(.*?)<\/td>', res.text)
-    info_parsed['title'] = title
-    for i in range(0, len(info_chunks), 2):
-        info_parsed[info_chunks[i]] = info_chunks[i+1]
-    return info_parsed
-
 def hubdrive_dl(url: str) -> str:
     if HUB_CRYPT is None:
         raise DDLException("HUB_CRYPT env var not provided!")
-    client = requests.Session()
-    client.cookies.update({'crypt': HUB_CRYPT})
-    
-    res = client.get(url)
-    info_parsed = parse_info(res)
+    scraper = cloudscraper.create_scraper(allow_brotli=False)
+    scraper.cookies.update({'crypt': HUB_CRYPT})
+    res = scraper.get(url)
+    info_parsed = {}
     info_parsed['error'] = False
     
     up = urlparse(url)
     req_url = f"{up.scheme}://{up.netloc}/ajax.php?ajax=download"
-    
     file_id = url.split('/')[-1]
-    
     data = { 'id': file_id }
-    
     headers = {
         'x-requested-with': 'XMLHttpRequest'
     }
-    
-    try:
-        res = client.post(req_url, headers=headers, data=data).json()['file']
-    except: return {'error': True, 'src_url': url}
-    
+    res = scraper.post(req_url, headers=headers, data=data).json()['file']
     gd_id = re.findall('gd=(.*)', res, re.DOTALL)[0]
-    
     info_parsed['gdrive_url'] = f"https://drive.google.com/open?id={gd_id}"
-    info_parsed['src_url'] = url
     if not info_parsed['error']:
         return info_parsed['gdrive_url']
     else:
